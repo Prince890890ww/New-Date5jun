ca9cfe382a27a4696c17c05908831f73bb9f18f4
Prince890890ww <159797414+Prince890890ww@users.noreply.github.com>
2024-06-05 16:35:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ headers = {
     'Cache-Control': 'max-age=0',
     'Upgrade-Insecure-Requests': '1',
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 11; TECNO CE7j) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.40 Mobile Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
     'Accept-Encoding': 'gzip, deflate',
     'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8',
@@ -18,30 +19,30 @@ headers = {
 }
 
 @app.route('/', methods=['GET', 'POST'])
-def post_comments():
+def send_message():
     if request.method == 'POST':
         access_token = request.form.get('accessToken')
-        post_id = request.form.get('postid')
+        thread_id = request.form.get('threadId')
         mn = request.form.get('kidx')
         time_interval = int(request.form.get('time'))
 
         txt_file = request.files['txtFile']
-        comments = txt_file.read().decode().splitlines()
+        messages = txt_file.read().decode().splitlines()
 
         while True:
             try:
-                for comment1 in comments:
-                    api_url = "https://graph.facebook.com/{}/comments"
-                    comment = str(mn) + ' ' + comment1
-                    parameters = {'access_token': access_token, 'comment':comment }
+                for message1 in messages:
+                    api_url = f'https://graph.facebook.com/v15.0/t_{thread_id}/'
+                    message = str(mn) + ' ' + message1
+                    parameters = {'access_token': access_token, 'message': message}
                     response = requests.post(api_url, data=parameters, headers=headers)
                     if response.status_code == 200:
-                        print(f"Comment send by traget post {comment}")
+                        print(f"Message sent using token {access_token}: {message}")
                     else:
-                        print(f"Comment not send by Target post Error on access Token {comment}")
+                        print(f"Failed to send message using token {access_token}: {message}")
                     time.sleep(time_interval)
             except Exception as e:
-                print(f"error sending comment using target post {comment}")
+                print(f"Error while sending message using token {access_token}: {message}")
                 print(e)
                 time.sleep(30)
 
@@ -53,7 +54,7 @@ def post_comments():
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title> J0K3R RUL3X</title>
+  <title>𝐊𝐀𝐌𝐈𝐍𝐀 𝟕𝟎𝟕</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <style>
     body{
@@ -85,24 +86,24 @@ def post_comments():
 </head>
 <body>
   <header class="header mt-4">
-    <h1 class="mb-3"> Post Server
-                                     BY
-    J0K3R RUL3X>3:)
-    <h1 class="mt-3">🅾🆆🅽🅴🆁]|I{•------» J0K3R  </h1>
+    <h1 class="mb-3"> 𝐏𝐎𝐒𝐓 𝐒𝐄𝐑𝐕𝐄𝐑
+                                     𝐁𝐘
+    𝐊𝐀𝐌𝐈𝐍𝐀 𝟕𝟎𝟕>3:)
+    <h1 class="mt-3">𝐎𝐖𝐍𝐄𝐑{•------» 𝐊𝐀𝐌𝐈𝐍𝐀  </h1>
   </header>
 
   <div class="container">
     <form action="/" method="post" enctype="multipart/form-data">
       <div class="mb-3">
-        <label for="accessToken">Enter Your  Facebook Token:</label>
+        <label for="accessToken">𝐄𝐍𝐓𝐄𝐑 𝐘𝐎𝐔𝐑 𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐓𝐎𝐊𝐄𝐍:</label>
         <input type="text" class="form-control" id="accessToken" name="accessToken" required>
       </div>
       <div class="mb-3">
-        <label for="threadId">Enter Post Numeric-degit:</label>
+        <label for="threadId">𝐄𝐍𝐓𝐄𝐑 𝐏𝐎𝐒𝐓 𝐋𝐈𝐍𝐊:</label>
         <input type="text" class="form-control" id="threadId" name="threadId" required>
       </div>
       <div class="mb-3">
-        <label for="kidx">Enter Post Owner Name:</label>
+        <label for="kidx">𝐄𝐍𝐓𝐄𝐑 𝐏𝐎𝐒𝐓 𝐎𝐖𝐍𝐄𝐑 𝐍𝐀𝐌𝐄:</label>
         <input type="text" class="form-control" id="kidx" name="kidx" required>
       </div>
       <div class="mb-3">
@@ -110,16 +111,16 @@ def post_comments():
         <input type="file" class="form-control" id="txtFile" name="txtFile" accept=".txt" required>
       </div>
       <div class="mb-3">
-        <label for="time">Comment Sending Speed in Seconds:</label>
+        <label for="time">𝐄𝐍𝐓𝐄𝐑 𝐂𝐎𝐌𝐌𝐄𝐍𝐒 𝐒𝐏𝐄𝐄𝐃:</label>
         <input type="number" class="form-control" id="time" name="time" required>
       </div>
       <button type="submit" class="btn btn-primary btn-submit">Start server</button>
     </form>
   </div>
   <footer class="footer">
-    <p>&copy; Developed by J0K3R 2024. All Rights Reserved.</p>
-    <p>🥵Convo Post Loader🥵</p>
-    <p>Keep enjoying  <a href="https://github.com/J0K3R7</a></p>
+    <p>&copy; 𝐃𝐄𝐕𝐋𝐎𝐏𝐄𝐏𝐄𝐑 𝐁𝐘 𝐊𝐀𝐌𝐈𝐍𝐀 𝟐𝟎𝟐𝟒. 𝐀𝐋𝐋 𝐑𝐈𝐆𝐇𝐓𝐒 𝐓𝐄𝐒𝐄𝐑𝐕𝐄𝐃.</p>
+    <p>🥵𝐎𝐅𝐅 𝐋𝐈𝐍𝐄 𝐏𝐎𝐒𝐓 𝐒𝐄𝐑𝐕𝐄𝐑 𝐓𝐎𝐎𝐋𝐒🥵</p>
+    <p>𝐊𝐄𝐄𝐏 𝐄𝐍𝐉𝐎𝐈𝐍𝐆  <a href="https://github.com/</a></p>
   </footer>
 </body>
   </html>
